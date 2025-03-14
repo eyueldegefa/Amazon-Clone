a4d2aa65b6ba6566df474d947b97ef7354ffba31
eyueldegefa <you@example.com>
2025-03-15 00:57:57 +0300

    Cart Part

--- amazone-clone/src/Pages/Cart/Cart.jsx
+++ amazone-clone/src/Pages/Cart/Cart.jsx
@@ -1,16 +1,34 @@
-import React, { useContext } from 'react'
-import Layout from '../../components/LayOut/Layout'
-import { DataContext } from '../../components/DetailProvider/DetailProvider'
-import ProductCard from '../../components/Product/ProductCard'
-import classes from './Cart.module.css'
-import CurrencyFormat from '../../components/CurrencyFormat/CurrencyFormat'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
+import Layout from '../../components/LayOut/Layout';
+import { DataContext } from '../../components/DetailProvider/DetailProvider';
+import ProductCard from '../../components/Product/ProductCard';
+import classes from './Cart.module.css';
+import CurrencyFormat from '../../components/CurrencyFormat/CurrencyFormat';
+import { Link } from 'react-router-dom';
+import {Type} from '../../Utility/actionType';
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 
 function Cart() {
   const [{basket, user}, dispatch] = useContext(DataContext);
   const total = basket.reduce((amount,item)=>{
       return  item.price * item.amount  + amount
   },0)
+
+  const increment = (item)=>{
+    dispatch({
+      type:Type.ADD_TO_BASKET,
+      item
+    })
+  }
+
+  const decrement = (id)=>{
+    dispatch({
+      type:Type.REMOVE_FROM_BASKET,
+      id
+    })
+  }
+
   return (
     <Layout>
         <section className={`${classes.container}`}>
@@ -21,13 +39,21 @@ function Cart() {
             {
               basket?.length==0?(<p>Opps! No item in your cart</p>):(
                 basket?.map((item,i)=>{
-                  return <ProductCard 
-                  key={i}
-                    product={item}
-                    renderDesc={true}
-                    flex={true}
-                    renderAdd={false}
-                  />
+                  return <section className='d-flex'>
+                    <ProductCard 
+                    key={i}
+                      product={item}
+                      renderDesc={true}
+                      flex={true}
+                      renderAdd={false}
+                    />
+                    {/* Buttons to increase and decrease the items */}
+                    <div className={classes.btn_container}>
+                      <button className={classes.btn} onClick={()=>increment(item)}><IoIosArrowUp size={30}/></button>
+                      <span>{item.amount}</span>
+                      <button className={classes.btn} onClick={()=>decrement(item.id)}><IoIosArrowDown size={30}/></button>
+                    </div>
+                  </section>
                 })
               )
             }
