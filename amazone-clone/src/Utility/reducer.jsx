4a6a8252e38f70f540157d32a464a6b7b4e70c82
eyueldegefa <you@example.com>
2025-03-15 21:12:57 +0300

    Cart Finished

--- amazone-clone/src/Utility/reducer.jsx
+++ amazone-clone/src/Utility/reducer.jsx
@@ -1,22 +1,22 @@
-import {Type} from './actionType'
+import { Type } from './actionType'
 
 export const initialState = {
     basket: []
 }
 
-export const reducer = (state, action)=>{
+export const reducer = (state, action) => {
     switch (action.type) {
         case Type.ADD_TO_BASKET:
             // Check if the item is already in the basket
-            const existingItem = state.basket.findIndex((basketItem) => basketItem.id === action.item.id);
-            if(!existingItem){
+            const existingItemIndex = state.basket.findIndex((basketItem) => basketItem.id === action.item.id);
+            if (existingItemIndex === -1) {
                 return {
                     ...state,
-                    basket: [...state.basket, {...action.item, amount: 1}]
+                    basket: [...state.basket, { ...action.item, amount: 1 }]
                 }
-            }else{
+            } else {
                 const updatedBasket = state.basket.map((item) => {
-                    return item.id === action.item.id ? {...item, amount: item.amount + 1} : item
+                    return item.id === action.item.id ? { ...item, amount: item.amount + 1 } : item
                 })
                 return {
                     ...state,
@@ -26,10 +26,10 @@ export const reducer = (state, action)=>{
         case Type.REMOVE_FROM_BASKET:
             const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
             let newBasket = [...state.basket];
-            if(index >= 0){
-                if(newBasket[index].amount > 1){
+            if (index >= 0) {
+                if (newBasket[index].amount > 1) {
                     newBasket[index].amount -= 1;
-                }else{
+                } else {
                     newBasket.splice(index, 1);
                 }
             }
